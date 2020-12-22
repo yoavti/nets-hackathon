a3190f31fe45c3da8990719a958e4da6b0da20f0
yoavt <yoavti@post.bgu.ac.il>
2020-12-22 18:31:20 +0200

    added sending and receiving of team name by the server and client

--- client.py
+++ client.py
@@ -1,21 +1,28 @@
-from offer_message import unpack_offer
 from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
+from offer_message import unpack_offer
 
 
 OFFER_PORT = 13117
 BUFFER_SIZE = 2048
+TEAM_NAME = b'Silverhand\n'
 
 
 if __name__ == "__main__":
+    print('Client started, listening for offer requests...')
     with socket(AF_INET, SOCK_DGRAM) as offer_socket:
         offer_socket.bind(('', OFFER_PORT))
         while True:
-            # Looking for server and connecting to it
+            # Looking for server
             message, server_address = offer_socket.recvfrom(BUFFER_SIZE)
             server_port = unpack_offer(message)
             if not server_port:
                 pass
+            # Connecting to a server
+            print(
+                f'Received offer from {server_address}, attempting to connect...')
             with socket(AF_INET, SOCK_STREAM) as game_socket:
                 game_socket.connect((server_address, server_port))
+                game_socket.send(TEAM_NAME)
             # Game mode
+            # play game
             # After finishing game...
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from time import sleep
-from socket import socket, AF_INET, SOCK_DGRAM
+from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
 from offer_message import pack_offer
 
 
@@ -7,19 +7,27 @@ HOST = '172.1.0.77'
 NETWORK = '172.1.0/24'
 OFFER_PORT = 13117
 GAME_PORT = 12000
+BACKLOG = 1
+BUFFER_SIZE = 2048
 
 
 if __name__ == "__main__":
     print(f'Server started, listening on IP address {HOST}')
-    while True:
-        # Waiting for clients
-        with socket(AF_INET, SOCK_DGRAM) as offer_socket:
-            for i in range(10):
-                offer_socket.sendto(
-                    pack_offer(GAME_PORT),
-                    (NETWORK, OFFER_PORT))
-                sleep(1)
-        # Game mode
-        # manage game
-        # After 10 seconds...
-        pass
+    with socket(AF_INET, SOCK_STREAM) as game_socket:
+        game_socket.bind(('', GAME_PORT))
+        game_socket.listen(BACKLOG)
+        while True:
+            # Waiting for clients
+            with socket(AF_INET, SOCK_DGRAM) as offer_socket:
+                for i in range(10):
+                    offer_socket.sendto(
+                        pack_offer(GAME_PORT),
+                        (NETWORK, OFFER_PORT))
+                    client_socket, client_address = game_socket.accept()
+                    with client_socket:
+                        team_name = client_socket.recv(BUFFER_SIZE)
+                        team_name = team_name.decode('utf-8').rstrip()
+                    sleep(1)
+            # Game mode
+            # manage game
+            # After 10 seconds...
