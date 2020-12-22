41b6d089b0890f3cf340c98d625a8d748a26edef
yoavt <yoavti@post.bgu.ac.il>
2020-12-22 21:34:14 +0200

    implemented player side of gameplay

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
 from offer_message import unpack_offer
+from keyboard import read_key
 
 
 OFFER_PORT = 13117
@@ -23,6 +24,12 @@ if __name__ == "__main__":
         with socket(AF_INET, SOCK_STREAM) as game_socket:
             game_socket.connect((server_address, server_port))
             game_socket.send(TEAM_NAME)
-        # Game mode
-        # play game
-        # After finishing game...
+            # Game mode
+            start_message = game_socket.recv(BUFFER_SIZE)
+            print(start_message.decode('utf-8'))
+            while True:
+                try:
+                    key = read_key()
+                    game_socket.send(key)
+                except:
+                    break
