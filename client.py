2e43bfd2431ee17688acbb9b9384086c4c8dbdca
yoavt <yoavti@post.bgu.ac.il>
2020-12-27 00:17:29 +0200

    fixed bugs, added error handling, and added pretty printing

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
-from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM, SOL_SOCKET, SO_REUSEPORT, SO_BROADCAST
 from string_message import BUFFER_SIZE, send_string, recv_string
 from offer_message import unpack_offer
 from multiprocessing import Process
+from ANSI import bcolors, annotate_variable, print_error
 import getch
 
 
@@ -10,52 +11,71 @@ OFFER_PORT = 13117
 
 
 def send_keys(sock):
+    'Method given to a process which is responsible for sending keystrokes to the server'
     while True:
+        key = getch.getch()
         try:
-            key = getch.getch()
             send_string(sock, key)
         except:
             break
 
 
-def receive_messages(sock):
-    while True:
+def client_round():
+    'Method handling a single round of the client'
+    # Looking for server
+    print('Listening for offer requests')
+    with socket(AF_INET, SOCK_DGRAM) as offer_socket:
+        offer_socket.setsockopt(SOL_SOCKET, SO_REUSEPORT, 1)
+        offer_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+        offer_socket.bind(('', OFFER_PORT))
+        message, server_address = offer_socket.recvfrom(BUFFER_SIZE)
+    server_port = unpack_offer(message)
+    server_ip, _ = server_address
+    if not server_port:
+        print_error('Incorrect offer message format received')
+        return
+    # Connecting to a server
+    print(
+        f'Received offer from {annotate_variable(server_ip)}, attempting to connect...')
+    with socket(AF_INET, SOCK_STREAM) as game_socket:
+        # Connecting to server
         try:
-            server_message = recv_string(sock)
-            print(server_message)
+            game_socket.connect((server_ip, server_port))
         except:
-            break
+            print_error('Failed to connect to server')
+            return
+        # Sending team name (with \n at the end)
+        try:
+            send_string(game_socket, TEAM_NAME + '\n')
+        except:
+            print_error('Failed to send team name')
+            return
+        print("Connected and waiting for game to start")
+        # Game mode
+        try:
+            start_message = recv_string(game_socket)
+            if not start_message:
+                print_error('Could not receive start message for the game')
+                return
+            print(start_message)
+        except:
+            print_error('Could not receive start message for the game')
+            return
+        key_sender = Process(target=send_keys, args=(game_socket,))
+        key_sender.start()
+        while True:
+            try:
+                server_message = recv_string(game_socket)
+                if not server_message:
+                    break
+                print(server_message)
+            except:
+                break
+        key_sender.terminate()
+        print('Server disconnected')
 
 
 if __name__ == "__main__":
-    print('Client started, listening for offer requests...')
+    print('Client started')
     while True:
-        # Looking for server
-        with socket(AF_INET, SOCK_DGRAM) as offer_socket:
-            offer_socket.bind(('', OFFER_PORT))
-            message, server_address = offer_socket.recvfrom(BUFFER_SIZE)
-        server_port = unpack_offer(message)
-        server_ip, _ = server_address
-        if not server_port:
-            break
-        # Connecting to a server
-        print(
-            f'Received offer from {server_ip}, attempting to connect...')
-        with socket(AF_INET, SOCK_STREAM) as game_socket:
-            game_socket.connect((server_ip, server_port))
-            send_string(game_socket, TEAM_NAME + '\n')
-            print("Connected and waiting for game to start")
-            # Game mode
-            start_message = recv_string(game_socket)
-            print(start_message)
-            key_sender = Process(target=send_keys, args=(game_socket,))
-            key_sender.start()
-            while True:
-                try:
-                    server_message = recv_string(game_socket)
-                    print(server_message)
-                    break
-                except:
-                    key_sender.terminate()
-                    break
-            print('Server disconnected, listening for offer requests...')
+        client_round()
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM, SOL_SOCKET, SO_REUSEPORT, SO_BROADCAST
 from string_message import BUFFER_SIZE, send_string, recv_string
 from offer_message import pack_offer
 from time import sleep, time
@@ -6,10 +6,12 @@ from random import choice
 from multiprocessing import Process, Queue
 from scapy.all import get_if_addr
 from recordtype import recordtype
+from ANSI import bcolors, annotate_variable, annotate_name, annotate_underline, print_error, print_warning
+from time import time
 
 
-NETWORK = '127.0.0.1'
-HOST = '127.0.0.1'  # get_if_addr(NETWORK)
+NETWORK = '255.255.255.255'
+HOST = '127.0.0.1'  # get_if_addr('eth1')
 OFFER_PORT = 13117
 BACKLOG = 1
 DURATION = 10
@@ -28,40 +30,58 @@ def manage_player(player, q):
     and increasing their score.
     """
     while True:
-        msg = player.socket.recv(BUFFER_SIZE)
-        q.put((player.name, msg))
+        try:
+            msg = recv_string(player.socket)
+            if not msg:
+                print_error(
+                    f'Failed to receive message from client {annotate_name(player.name)}')
+                break
+            if len(msg) == 1:
+                q.put((player.name, msg))
+        except:
+            print_error(
+                f'Failed to receive message from client {annotate_name(player.name)}')
+            break
 
 
 def send_offer(offer_socket, port):
     'Method given to a process charged with sending offer messages through the given socket every second'
     while True:
         # Sending offer to all clients in the network
-        offer_socket.sendto(
-            pack_offer(port),
-            (NETWORK, OFFER_PORT))
+        try:
+            offer_socket.sendto(
+                pack_offer(port),
+                (NETWORK, OFFER_PORT))
+        except:
+            print_warning('Failed to send offer messages')
+            pass
         sleep(DELAY_BETWEEN_OFFERS)
 
 
 if __name__ == "__main__":
-    print(f'Server started, listening on IP address {HOST}')
+    print(
+        f'Server started, listening on IP address {annotate_variable(HOST)}')
     while True:
         players = []
         # Waiting for clients
+        print('Sending out offer requests')
         with socket(AF_INET, SOCK_STREAM) as join_socket:
             join_socket.bind(('', 0))
             join_socket.listen(BACKLOG)
             join_socket.settimeout(DURATION)
             with socket(AF_INET, SOCK_DGRAM) as offer_socket:
+                offer_socket.setsockopt(SOL_SOCKET, SO_REUSEPORT, 1)
+                offer_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
                 join_port = join_socket.getsockname()[1]
                 offer_sender = Process(
                     target=send_offer,
                     args=(offer_socket, join_port,))
                 offer_sender.start()
                 # Waiting for clients to respond
-                while True:
+                start_time = time()
+                while time() - start_time < DURATION:
                     try:
                         client_socket, client_address = join_socket.accept()
-                        # Waiting for accepted client to sent their team name
                         team_name = recv_string(client_socket).rstrip()
                         # Setting some auxilliary parameters
                         team = choice(TEAMS)
@@ -80,26 +100,34 @@ if __name__ == "__main__":
         def player_names_of_team(team):
             'Returns the names of all players in the team joined by \\n.'
             return '\n'.join([
-                player.name
+                annotate_name(player.name)
                 for player in players
                 if player.team == team])
         # Game mode
         # Sending start message to all registered clients
         members_string = '\n'.join([
-            f"""
-            Group {team}:
-            ==
-            {player_names_of_team(team)}
-            """
+            '\n'.join([
+                f'Group {annotate_variable(team)}:',
+                f'{player_names_of_team(team)}'
+            ])
             for team in TEAMS])
-        start_message = f"""
-        Welcome to Keyboard Spamming Battle Royale.
-        {members_string}
-        Start pressing keys on your keyboard as fast as you can!
-        """
+        start_message = '\n'.join([
+            'Welcome to Keyboard Spamming Battle Royale.',
+            f'{members_string}',
+            'Start pressing keys on your keyboard as fast as you can!'
+        ])
         print(start_message)
-        for player in players:
-            send_string(player.socket, start_message)
+
+        def try_sending_start_message(player, start_message):
+            try:
+                send_string(player.socket, start_message)
+                return True
+            except:
+                return False
+        players = [
+            player
+            for player in players
+            if try_sending_start_message(player, start_message)]
         # Starting game by starting processes which will deal with each clients key-mashing
         q = Queue()
         processes = [
@@ -113,12 +141,26 @@ if __name__ == "__main__":
         for p in processes:
             p.terminate()
         # Post-game analysis
-        score_per_player = {player.name: 0 for player in players}
+        key_histogram = {}
         while not q.empty():
             name, key = q.get()
+            if key in key_histogram:
+                key_histogram[key] += 1
+            else:
+                key_histogram[key] = 0
             for player in players:
                 if player.name == name:
                     player.score += 1
+        ordered_players = sorted(
+            players,
+            key=lambda player: player.score,
+            reverse=True)
+        leaderboard = [
+            f'{annotate_name(player.name)}\t{annotate_variable(player.score)}'
+            for player in ordered_players]
+        leaderboard[0] = annotate_underline(leaderboard[0])
+        leaderboard_string = '\n'.join(leaderboard)
+        common_key = max(key_histogram, key=key_histogram.get)
         scores = [
             sum([
                 player.score
@@ -128,20 +170,27 @@ if __name__ == "__main__":
         winner = 1 + scores.index(max(scores))
         # Sending end message to all registered clients
         scores_string = ' '.join([
-            f'Group {index + 1} typed in {score} characters.'
+            f'Group {annotate_variable(index + 1)} typed in {annotate_variable(score)} characters.'
             for index, score in enumerate(scores)])
-        end_message = f"""
-        Game over!
-        {scores_string}
-        Group {winner} wins!
-        Congratulations to the winners:
-        ==
-        {player_names_of_team(winner)}
-        """
+        end_message = '\n'.join([
+            'Game over!',
+            f'{scores_string}',
+            f'Group {annotate_variable(winner)} wins!',
+            'Congratulations to the winners:',
+            f'{player_names_of_team(winner)}',
+            'Leaderboard for this round:',
+            f'{leaderboard_string}',
+            f'The most commonly typed character was {annotate_name(common_key)}'
+        ])
         print(end_message)
         for player in players:
-            send_string(player.socket, end_message)
+            try:
+                send_string(player.socket, end_message)
+            except:
+                print_warning(
+                    f'Could not send end message to {annotate_name(player.name)}')
+                pass
         # Closing the TCP connections
         for player in players:
             player.socket.close()
-        print('​Game over, sending out offer requests...')
+        print('​Game over')
