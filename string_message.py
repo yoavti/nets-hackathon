420fb839e7f2e1cf10484e74ee29a1bc42ca2a1d
yoavt <yoavti@post.bgu.ac.il>
2020-12-29 18:51:47 +0200

    removed redundant encoding format of string messages

--- string_message.py
+++ string_message.py
@@ -1,5 +1,4 @@
 BUFFER_SIZE = 2048
-ENCODING_FORMAT = 'utf-8'
 
 
 def send_string(sock, message):
@@ -7,7 +6,7 @@ def send_string(sock, message):
     Encode the given string using utf-8,
     and sent it to the given socket.
     """
-    return sock.send(message.encode(ENCODING_FORMAT))
+    return sock.send(message.encode())
 
 
 def recv_string(sock):
@@ -15,4 +14,4 @@ def recv_string(sock):
     Receive data from the given socket,
     and decode it using utf-8.
     """
-    return sock.recv(BUFFER_SIZE).decode(ENCODING_FORMAT)
+    return sock.recv(BUFFER_SIZE).decode()
